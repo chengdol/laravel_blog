682cf1e842df225161e70aadb4a5e8c92f610d7f
chengdol <chengdol@usc.edu>
2017-09-19 14:23:35 -0700

    seed category table

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Category;
 class CategorySeeder extends Seeder
 {
     /**
@@ -11,6 +11,22 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        // some init category objects to seed table
+        $category = new Category();
+        $category->name = "Java";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Php";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Python";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Leetcode";
+        $category->save();
+
     }
 }
