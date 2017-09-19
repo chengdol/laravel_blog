219374bcc3bd50bbacf4c7c57e5e6eccdfff8bc4
chengdol <chengdol@usc.edu>
2017-09-19 14:19:12 -0700

    set relationship among tables

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Post;
 class Category extends Model
 {
-    //
+    // define relationship with posts table
+    public function posts()
+    {
+        // assign pivot table name
+        return $this->belongsToMany('Post', 'posts_categories');
+    }
 }
